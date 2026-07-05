be1635a46160f1fbff40aa187c7a14f09dfdeb24
jgastecker <jonas.gastecker@gmail.com>
2026-07-05 16:21:23 +0200

    update

--- hooks/post_install.lua
+++ hooks/post_install.lua
@@ -1,4 +1,10 @@
 function PLUGIN:PostInstall(ctx)
+    -- Create the destination bin directory
+    os.execute("mkdir -p " .. ctx.path .. "/bin")
+    -- Copy the scripts from the plugin repository directory to the install path
+    os.execute("cp -r bin/* " .. ctx.path .. "/bin/")
+    os.execute("chmod +x " .. ctx.path .. "/bin/*")
+
     local script_path = ctx.path .. "/bin/qemu-dind-setup"
     print("Executing initialization script (this takes a while)...")
     local status = os.execute(script_path)
--- hooks/pre_install.lua
+++ hooks/pre_install.lua
@@ -1,7 +1,4 @@
 function PLUGIN:PreInstall(ctx)
-    os.execute("mkdir -p " .. ctx.path .. "/bin")
-    os.execute("cp -r bin/* " .. ctx.path .. "/bin/")
-    os.execute("chmod +x " .. ctx.path .. "/bin/*")
     return {
         version = ctx.version
     }
