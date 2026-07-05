e8da17a8ae23c97c60170e1ad5e799778a7de3c2
jgastecker <jonas.gastecker@gmail.com>
2026-07-05 22:38:58 +0200

    update

--- build/create.sh
+++ build/create.sh
@@ -16,7 +16,9 @@ virt-customize -a alpine-docker.qcow2 \
    --install openssh,docker \
    --run-command "rc-update add sshd default" \
    --run-command "rc-update add docker default" \
+   --run-command "sed -i '/^[# \t]*PermitRootLogin/d' /etc/ssh/sshd_config" \
    --run-command "echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config" \
+   --run-command "sed -i '/^[# \t]*AllowTcpForwarding/d' /etc/ssh/sshd_config" \
    --run-command "echo 'AllowTcpForwarding yes' >> /etc/ssh/sshd_config" \
    --run-command "echo 'DOCKER_OPTS=\"-H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock\"' >> /etc/conf.d/docker" \
    --run-command "echo 'net.ipv4.ip_local_port_range = 30000 30020' >> /etc/sysctl.conf" \
